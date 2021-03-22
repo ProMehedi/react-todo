9d9b7c8f4f05a300b262216a92f91167962c1dfe
promehedi <mmhasan563@gmail.com>
2021-03-23 00:13:05 +0600

    Not Found Handling

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -2,10 +2,17 @@ import React from 'react'
 import ListItem from './ListItem'
 
 const Lists = ({ todos, delHandler, doneHandler }) => {
+  if (todos.length <= 0) {
+    return (
+      <div className='jumbotron'>
+        <h1 className='display-5 text-center text-danger'>No Todo Found!</h1>
+      </div>
+    )
+  }
+
   return (
     <div className='listWrap'>
       <ul className='list-group'>
-        {!todos && <h1>Not Found</h1>}
         {todos &&
           todos.length > 0 &&
           todos.map((todo) => (
